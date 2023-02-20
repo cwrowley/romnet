baebeb0cd928c93035a18380cf1f379bf87049f4
Clancy Rowley <cwrowley@princeton.edu>
2023-02-20 14:55:57 -0500

    Use List, Tuple, Dict type objects for py3.8 compatibility

--- src/romnet/typing.py
+++ src/romnet/typing.py
@@ -1,13 +1,8 @@
-from typing import Any, Callable, Union
+from typing import Any, Callable, List, Union
 
 import numpy as np
 import numpy.typing as npt
 
-# Vector = Union[npt.NDArray[np.float64], npt.NDArray[np.complex64]]
-# T = TypeVar("T", np.float64, np.complex64)
-# Vector = npt.NDArray[T]
-# Vector = npt.NDArray[np.float64]
-# Vector = npt.NDArray[Any]
-Vector = np.ndarray[Any, Any]
+Vector = npt.NDArray[Any]
 VectorField = Callable[[Vector], Vector]
-VectorList = Union[list[Vector], npt.NDArray[np.float64]]
+VectorList = Union[List[Vector], npt.NDArray[np.float64]]
