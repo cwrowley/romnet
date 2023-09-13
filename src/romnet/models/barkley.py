174bc51f78acbb9a3b214975220196e71b446e18
Clancy Rowley <cwrowley@princeton.edu>
2023-09-13 13:34:55 -0400

    Clean up some expressions in Barkley model

--- src/romnet/models/barkley.py
+++ src/romnet/models/barkley.py
@@ -1,33 +1,18 @@
-"""Kuramoto-Sivashinsky equation"""
+"""Barkley model for turbulent puffs
+
+D. Barkley, Theoretical perspective on the route to turbulence in a pipe,
+Journal of Fluid Mechanics 803 P1, 2016
+"""
 
 from typing import Callable
 
 import numpy as np
-import scipy as sp
 
 from .. import SemiLinearModel
 from ..typing import Vector
+from .ks import fft_multiply, freq_to_space, space_to_freq
 
 __all__ = ["BarkleyPuffModel", "freq_to_space", "space_to_freq"]
-
-
-def fft_multiply(yf: Vector, zf: Vector) -> Vector:
-    """Multiply two Fourier series using fast Fourier transform"""
-    nmodes = len(yf) - 1
-    y = sp.fft.irfft(yf)
-    z = sp.fft.irfft(zf)
-    yz = y * z
-    return 2 * nmodes * sp.fft.rfft(yz)
-
-
-def freq_to_space(yf: Vector) -> Vector:
-    nmodes = len(yf) - 1
-    return 2 * nmodes * sp.fft.irfft(yf)
-
-
-def space_to_freq(y: Vector) -> Vector:
-    nmodes = len(y) // 2
-    return sp.fft.rfft(y) / (2 * nmodes)
 
 
 class BarkleyPuffModel(SemiLinearModel):
@@ -55,12 +40,12 @@ class BarkleyPuffModel(SemiLinearModel):
         k = np.arange(nmodes + 1)
         ksq = (2 * np.pi / L) ** 2 * k**2
         self._deriv = (2j * np.pi / L) * k
+        self.N = nmodes + 1     # number of Fourier modes in each dependent variable
 
         # Linear dynamics
-        self._linear_factor_tl = -(delta+u0)*np.ones(self.nmodes+1) - D*ksq + zeta*self._deriv
-        self._linear_factor_bl = epsilon_2*ubar*np.ones(self.nmodes+1)
-        self._linear_factor_br = -epsilon_1*np.ones(self.nmodes+1) - Du*ksq
-
+        self._linear_tl = -(delta + u0) * np.ones(self.N) - D * ksq + zeta * self._deriv
+        self._linear_bl = epsilon_2 * ubar * np.ones(self.N)
+        self._linear_br = -epsilon_1 * np.ones(self.N) - Du * ksq
 
     def get_solver(self, alpha: float) -> Callable[[Vector], Vector]:
         def solver(u: Vector) -> Vector:
@@ -72,10 +57,12 @@ class BarkleyPuffModel(SemiLinearModel):
         # linear part is self-adjoint
         return self.get_solver(alpha)
 
-    def linear(self, u: Vector) -> Vector:
-        linear_component_q = self._linear_factor_tl*u[:self.nmodes+1]
-        linear_component_u = self._linear_factor_bl*u[:self.nmodes+1] + self._linear_factor_br*u[self.nmodes+1:]
-        return np.append(linear_component_q, linear_component_u)
+    def linear(self, state: Vector) -> Vector:
+        q = state[:self.N]
+        u = state[self.N:]
+        linear_q = self._linear_tl * q
+        linear_u = self._linear_bl * q + self._linear_br * u
+        return np.append(linear_q, linear_u)
 
     def adjoint(self, u: Vector) -> Vector:
         # linear part is self-adjoint
@@ -85,28 +72,42 @@ class BarkleyPuffModel(SemiLinearModel):
         # a * b_x
         return fft_multiply(a, self._deriv * b)
 
-    def nonlinear(self, u: Vector) -> Vector:
-        q_nonlinear = fft_multiply(u[self.nmodes+1:], u[:self.nmodes+1]-self._deriv*u[:self.nmodes+1]) - (self.r+self.delta)*(fft_multiply(fft_multiply(u[:self.nmodes+1],u[:self.nmodes+1]), u[:self.nmodes+1]) - 2*fft_multiply(u[:self.nmodes+1],u[:self.nmodes+1]))
-        u_nonlinear = space_to_freq(self.epsilon_1*self.u0*np.ones(2*self.nmodes)) - self.epsilon_2*fft_multiply(u[self.nmodes+1:], u[:self.nmodes+1]) - fft_multiply(u[self.nmodes+1:], self._deriv*u[self.nmodes+1:])
+    def nonlinear(self, state: Vector) -> Vector:
+        q = state[:self.N]
+        u = state[self.N:]
+        q_sq = fft_multiply(q, q)
+        q_nonlinear = (fft_multiply(u, q - self._deriv * q)
+                       - (self.r + self.delta) * (fft_multiply(q_sq, q) - 2 * q_sq))
+        u_nonlinear = (space_to_freq(self.epsilon_1 * self.u0 * np.ones(2 * self.nmodes))
+                       - self.epsilon_2 * fft_multiply(u, q)
+                       - fft_multiply(u, self._deriv * u))
         return np.append(q_nonlinear, u_nonlinear)
 
     def adjoint_nonlinear(self, u: Vector, w: Vector) -> Vector:
         return self.bilinear(w, u) - self.bilinear(u, w)
 
-    def noise(self, u: Vector) -> Vector:
-        noise_mu = 0
-        noise_sig = 1
-        noise_size = 2*self.nmodes
-        white_noise = space_to_freq(np.random.normal(noise_mu, noise_sig, noise_size))
-        multiple_noise = fft_multiply(white_noise, u[:self.nmodes+1])
-        return self.sigma*np.append(multiple_noise, np.zeros(self.nmodes+1))
+    def noise(self, state: Vector) -> Vector:
+        q = state[:self.N]
+        noise_mu = 0.0
+        noise_sig = 1.0
+        noise_npts = 2 * self.nmodes
+        white_noise = space_to_freq(np.random.normal(noise_mu, noise_sig, noise_npts))
+        multiple_noise = fft_multiply(white_noise, q)
+        return self.sigma * np.append(multiple_noise, np.zeros(self.N))
 
-    def linear_exp(self, u: Vector, alpha: float) -> Vector:
-        linear_component_q = np.exp(self._linear_factor_tl*alpha)*u[:self.nmodes+1]
-        linear_component_u = np.exp(self._linear_factor_br*alpha)*u[self.nmodes+1:] + u[:self.nmodes+1]*self._linear_factor_bl*(np.exp(alpha*self._linear_factor_tl)-np.exp(alpha*self._linear_factor_br))/(self._linear_factor_tl - self._linear_factor_br)
-        return np.append(linear_component_q, linear_component_u)
+    def linear_exp(self, state: Vector, alpha: float) -> Vector:
+        q = state[:self.N]
+        u = state[self.N:]
+        expL_q = np.exp(self._linear_tl * alpha) * q
+        expL_u = (np.exp(self._linear_br * alpha) * u
+                  + q * self._linear_bl
+                  * (np.exp(alpha * self._linear_tl) - np.exp(alpha * self._linear_br))
+                  / (self._linear_tl - self._linear_br))
+        return np.append(expL_q, expL_u)
 
-    def matrix_inv(self, u: Vector) -> Vector:
-        invAu_q_component = u[:self.nmodes+1]/self._linear_factor_tl
-        invAu_u_component = (u[self.nmodes+1:] - (u[:self.nmodes+1]*self._linear_factor_bl/self._linear_factor_tl))/self._linear_factor_br
-        return np.append(invAu_q_component, invAu_u_component)
+    def matrix_inv(self, state: Vector) -> Vector:
+        q = state[:self.N]
+        u = state[self.N:]
+        sol_q = q / self._linear_tl
+        sol_u = (u - q * self._linear_bl / self._linear_tl) / self._linear_br
+        return np.append(sol_q, sol_u)
